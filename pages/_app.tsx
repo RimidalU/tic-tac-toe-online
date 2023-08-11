379b53a77818baf289c9cf574bbb70e52e704efd
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-11 22:01:14 +0300

    feat: add Rubik font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import { Rubik } from "next/font/google";
+
 import "../styles/global.css";
 
+const rubik = Rubik({ subsets: ["latin"] });
+
 function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={rubik.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default App;
