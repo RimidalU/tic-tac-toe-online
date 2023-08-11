e4e6d2c52a52bb0c9aee692898a2c9f36e1dc36e
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-11 21:31:45 +0300

    feat: add logo to header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+import logoSrc from "./logo.png";
+
 export function Header() {
-  return <header>header</header>;
+  return (
+    <header className="flex h-24 items-center px-10 bg-orange-50 shadow-lg">
+      <Image src={logoSrc} alt="Logo" className="h-20 w-24 "/>
+      header
+    </header>
+  );
 }
