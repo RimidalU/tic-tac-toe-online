f25ecea7dac4d870c11490e6baf19a6aaaaec9e4
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-11 22:03:01 +0300

    fix: fix buttom text font-weight

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
     <header className="flex h-24 items-center px-10 bg-orange-50 shadow-lg">
       <Image src={logoSrc} alt="Logo" className="h-20 w-24" />
       <div className="w-px h-16 bg-slate-300 mx-6" />
-      <button className="w-44 bg-amber-400 hover:bg-amber-300 transition-colors text-indigo-600 rounded-lg px-5 py-2">
+      <button className="w-44 bg-amber-400 hover:bg-amber-300 transition-colors font-medium text-indigo-600 rounded-lg px-5 py-2">
         Play
       </button>
     </header>
