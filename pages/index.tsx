7b19e05ae986df8e3412e7555c8c7b5231c6c44d
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-11 19:36:41 +0300

    feat: implement base header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
+import { Header } from "../components/header";
+
 function HomePage() {
-  return <div className=" bg-slate-100 min-h-screen">HomePage</div>;
+  return (
+    <div className=" bg-slate-200 min-h-screen">
+      <Header />
+      HomePage
+    </div>
+  );
 }
 
 export default HomePage;
