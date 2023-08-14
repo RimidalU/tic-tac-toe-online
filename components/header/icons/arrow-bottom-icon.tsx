0c4a3610d2a18ea6b5c46add29b01fb204210ad4
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-14 12:27:55 +0300

    refactor: change arrow size

--- components/header/icons/arrow-bottom-icon.tsx
+++ components/header/icons/arrow-bottom-icon.tsx
@@ -1,8 +1,8 @@
 export function ArrowBottomIcon() {
   return (
     <svg
-      width="20"
-      height="11"
+      width="13"
+      height="7"
       viewBox="0 0 13 7"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
