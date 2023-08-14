d84c2e4f463308f09125f6d8c19e061acce821f8
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-14 13:45:47 +0300

    feat: implement game title component

---
+++ components/game/Game-title.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+import { ArrowLeftIcon } from "./icons/arrow-left-icon";
+import { TimerIcon } from "./icons/timer-icon";
+import { StarIcon } from "./icons/star-icon";
+import { UserIcon } from "./icons/user-icon";
+
+export function GameTitle() {
+  return (
+    <section className="pl-2">
+      <Link
+        href="#"
+        className="flex items-center gap-2 hover:text-indigo-400 text-lg pb-3"
+      >
+        <ArrowLeftIcon />
+        To Home
+      </Link>
+      <h1 className=" text-5xl font-semibold leading-tight bg-gradient-to-r from-amber-400 via-indigo-600 to-red-500 bg-clip-text text-transparent">
+        Tic Tac Toe Online
+      </h1>
+      <article className="flex items-center gap-4 text-xl font-light text-stone-600">
+        <StarIcon />
+        <div className="flex items-center gap-2">
+          <UserIcon />2
+        </div>
+        <div className="flex items-center gap-2">
+          <TimerIcon /> 1 minute per turn
+        </div>
+      </article>
+    </section>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { Footer } from "../components/footer";
+import { GameTitle } from "../components/game";
 import { Header } from "../components/header";
 
 function HomePage() {
   return (
     <div className="max-w-7xl mx-auto min-h-screen flex flex-col justify-between">
       <Header />
-      <main className="pt-6 max-w-2xl border-2">game</main>
+      <main className="pt-6 max-w-2xl flex-1 mx-auto">
+        <GameTitle />
+      </main>
       <Footer />
     </div>
   );
