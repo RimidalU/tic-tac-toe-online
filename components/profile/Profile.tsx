766454f110b679d47e1a06c5b3f907c6d06e3e26
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-15 17:58:13 +0300

    fix: change styles

--- components/profile/Profile.tsx
+++ components/profile/Profile.tsx
@@ -8,8 +8,10 @@ export function Profile({ className }: HTMLAttributes<string>) {
   return (
     <article className={clsx(className, "flex items-center gap-4")}>
       <Image src={avatarSrc} width={48} height={48} alt="Avatar" />
-      <div className="truncate">
-        <div className="font-semibold text-xl leading-tight truncate">UserName</div>
+      <div className="overflow-hidden">
+        <div className="font-semibold text-xl leading-tight truncate">
+          UserName
+        </div>
         <div className="text-stone-600">Rating: 1422</div>
       </div>
     </article>
