68f9c487815db11b0255a4ab43ebbfe2ec917b04
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-19 15:06:51 +0300

    refactor: add fake players

--- components/refactor-game/constants.ts
+++ components/refactor-game/constants.ts
@@ -1,3 +1,8 @@
+import Avatar1Icon from "./ui/images/avatar-1.png";
+import Avatar2Icon from "./ui/images/avatar-2.png";
+import Avatar3Icon from "./ui/images/avatar-3.png";
+import Avatar4Icon from "./ui/images/avatar-4.png";
+
 export const GAME_SYMBOLS = {
   CROSS: "cross",
   ZERO: "zero",
@@ -10,4 +15,35 @@ export const MOVE_ORDER = [
   GAME_SYMBOLS.ZERO,
   GAME_SYMBOLS.SQUARE,
   GAME_SYMBOLS.TRIANGLE,
+];
+
+export const PLAYERS = [
+  {
+    id: 1,
+    name: "Red Bull",
+    rating: 1243,
+    avatar: Avatar1Icon,
+    symbol: GAME_SYMBOLS.CROSS,
+  },
+  {
+    id: 2,
+    name: "Harley Quinn",
+    rating: 2345,
+    avatar: Avatar2Icon,
+    symbol: GAME_SYMBOLS.ZERO,
+  },
+  {
+    id: 3,
+    name: "Dog",
+    rating: 1743,
+    avatar: Avatar3Icon,
+    symbol: GAME_SYMBOLS.SQUARE,
+  },
+  {
+    id: 4,
+    name: "Yoda",
+    rating: 273,
+    avatar: Avatar4Icon,
+    symbol: GAME_SYMBOLS.TRIANGLE,
+  },
 ];
