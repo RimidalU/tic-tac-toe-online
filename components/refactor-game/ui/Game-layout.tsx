0f2d2c7862d9eba6b6891d685438b9f2e4beff49
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-19 11:37:22 +0300

    refactor: add wrapper to game layout

--- components/refactor-game/ui/Game-layout.tsx
+++ components/refactor-game/ui/Game-layout.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 export function GameLayout({
   backLink,
   appTitle,
@@ -8,10 +10,12 @@ export function GameLayout({
   gameInfo: JSX.Element;
 }) {
   return (
-    <section className="pl-2">
-      {backLink}
-      {appTitle}
-      {gameInfo}
+    <section className="bg-orange-50 shadow-lg px-8 py-6 mb-4">
+      <section className="pl-2">
+        {backLink}
+        {appTitle}
+        {gameInfo}
+      </section>
     </section>
   );
 }
