5268ef0a3fc164cc41615e73ca1a0ba066f32d49
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-19 12:53:07 +0300

    refactor: add GameSymbol component

---
+++ components/refactor-game/constants.ts
@@ -0,0 +1,13 @@
+export const GAME_SYMBOLS = {
+  CROSS: "cross",
+  ZERO: "zero",
+  TRIANGLE: "triangle",
+  SQUARE: "square",
+};
+
+export const MOVE_ORDER = [
+  GAME_SYMBOLS.CROSS,
+  GAME_SYMBOLS.ZERO,
+  GAME_SYMBOLS.SQUARE,
+  GAME_SYMBOLS.TRIANGLE,
+];
