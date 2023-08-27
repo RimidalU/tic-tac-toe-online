452846a7d4fb1922767a2edbe4c4f7319ad040bd
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-27 19:07:39 +0300

    feat: add UiInput to app

--- components/refactor-game/ui/New-game-modal.tsx
+++ components/refactor-game/ui/New-game-modal.tsx
@@ -1,4 +1,5 @@
 import { UiButton, UiModal } from "../../uikit";
+import { UiInput } from "../../uikit/fields/ui-input";
 
 export default function NewGameModal({ isOpen }: { isOpen: boolean }) {
   const handleClose = () => {
@@ -9,7 +10,17 @@ export default function NewGameModal({ isOpen }: { isOpen: boolean }) {
       <UiModal.Header>New Game</UiModal.Header>
       <UiModal.Body>
         <div className="text-2xl text-slate-600">Game settings:</div>
-        <form className="bg-orange-50 shadow-lg px-8 py-4 grid grid-cols-2 gap-3 justify-between mt-4"></form>
+        <form className="bg-orange-50 shadow-lg px-8 py-4 grid grid-cols-2 gap-3 justify-between mt-4">
+          <UiInput
+            label="Game turn time"
+            helperText="Insert game time in seconds"
+            placeholder="60"
+            required
+            defaultValue={60}
+            type="number"
+            min="0"
+          />
+        </form>
       </UiModal.Body>
       <UiModal.Footer>
         <UiButton size="md" variant="outline">
