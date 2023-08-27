ef999f4e53c86ae8fd883edc620cb3c31d5829a4
RimidalU <stankevichuuladimiru@gmail.com>
2023-08-27 23:01:14 +0300

    feat: implement ui-field-select component

---
+++ components/uikit/fields/ui-field-select.tsx
@@ -0,0 +1,49 @@
+import { SelectHTMLAttributes } from "react";
+import clsx from "clsx";
+
+import { IBoard_sizes } from "../../refactor-game/constants";
+
+/**
+ *
+ * @param {{
+ *   required?: boolean;
+ *   htmlId: string;
+ *   errorText?: string;
+ *   className?: string;
+ *   options: IBoard_sizes;
+ * } & import('react').SelectHTMLAttributes<HTMLSelectElement>} props
+ *
+ */
+
+export function UiFieldSelect({
+  required,
+  htmlId,
+  className,
+  errorText,
+  options,
+  ...inputProps
+}: {
+  required?: boolean;
+  htmlId: string;
+  errorText?: string;
+  options: IBoard_sizes;
+  className?: string;
+} & SelectHTMLAttributes<HTMLSelectElement>) {
+  return (
+    <select
+      id={htmlId}
+      required={required}
+      className={clsx(
+        `text-base px-4 py-2 block w-full rounded-md outline-0 border bg-slate-200 shadow-sm focus:ring 
+          disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-stone-300`,
+        errorText
+          ? " focus:border-red-500/60  focus:ring-red-500/60 border-red-300"
+          : " focus:border-indigo-600/60  focus:ring-indigo-600/60 border-gray-300",
+        className,
+      )}
+      {...inputProps}
+    >
+      {options?.map((item) => <option key={item.id}>{item.value}</option>)}
+    </select>
+  );
+}
